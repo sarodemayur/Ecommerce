3958d76b593cbe00aecdcacd96ec19a581773975
sarodemayur <167393223+sarodemayur@users.noreply.github.com>
2024-04-23 16:38:41 +0530

    App.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+import Sidebar from './components/Sidebar'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ProductsPage from './pages/ProductsPage';
+import About from "./pages/About"
+import Contact from './pages/Contact'
+import Signup from './pages/Signup'
+import Login from './pages/Login';
+//import ProtectedRoute from './Services/ProtectedRoute';
+import Cart from './components/Cart'
+import CheckOut from './pages/CheckOut';
+ 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <div className='overflow-hidden'></div>
+    <Router>
+      <Header/>
+      <Routes>
+        {/* <Route path='/' element={<ProtectedRoute/>}></Route> */}
+          <Route path='/' element={<Home/>}/>
+          <Route path='/product/:id' element={<ProductDetails/>}/>
+          <Route path='/productspage' element={<ProductsPage/>}/>
+          <Route path='cart' element={<Cart/>}/>
+          <Route path='checkout' element={<CheckOut/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/login' element={<Login/>}/>
+      </Routes>
+      <Sidebar/>
+      <Footer/>
+    </Router>
+    </>
   );
-}
+};
 
 export default App;
